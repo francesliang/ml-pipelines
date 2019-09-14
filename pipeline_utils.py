1a2229eb5ee2a6a415b7592d2c42f27f60745bbe
Xin <frances.xliang@gmail.com>
2019-09-14 17:16:04 +1000

    Add pipeline util module

---
+++ pipeline_utils.py
@@ -0,0 +1,17 @@
+
+_LABEL_KEYS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+def preprocessing_fn(inputs):
+    outputs = {}
+    outputs = inputs
+    print('outputs in preprocessing', outputs)
+    return outputs
+
+
+def trainer_fn(hparams, schema):
+    return {}
+
+
+def _build_estimator():
+    return
