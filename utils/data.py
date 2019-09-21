6af2efa44fbcadbd4ff50b000dabaa0af36cdec5
Xin <frances.xliang@gmail.com>
2019-09-21 17:47:16 +1000

    Update data util to store image data as nd-array in tfrecord

--- utils/data.py
+++ utils/data.py
@@ -1,40 +1,51 @@
 import os
 import tensorflow as tf
+import PIL
+import numpy as np
 
 
-def image_to_tfrecord(image_bytes, label):
-    image_shape = tf.image.decode_png(image_bytes).shape
+def image_to_tfrecord(image_file, label):
+    image = PIL.Image.open(image_file)
+    image_arr = np.asarray(image)
+    image_shape = image_arr.shape
+    h, w, = image_shape
+    d = 1
+
+    result = None
 
     feature = {
-        #'height': _int64_feature(image_shape[0]),
-        #'width': _int64_feature(image_shape[1]),
-        #'depth': _int64_feature(image_shape[2]),
-        'label': _int64_feature(label),
-        'image_raw': _bytes_feature(image_bytes)
+        'height': _int64_feature([h]),
+        'width': _int64_feature([w]),
+        'depth': _int64_feature([d]),
+        'label': _int64_feature([label]),
+        'image_raw': _int64_feature(image_arr.flatten())
     }
 
-    return tf.train.Example(features=tf.train.Features(feature=feature))
+    result = tf.train.Example(features=tf.train.Features(feature=feature))
+    return result
 
 
-def write_tfrecords(tfrecord_file, image_labels):
+def write_tfrecords(tfrecord_file, image_labels, num=10):
     with tf.io.TFRecordWriter(tfrecord_file) as writer:
-        for file_name, label in image_labels.items():
-            image_bytes = open(file_name, 'rb').read()
-            tf_example = image_to_tfrecord(image_bytes, label)
-            writer.write(tf_example.SerializeToString())
+        for file_name, label in list(image_labels.items())[:num]:
+            tf_example = image_to_tfrecord(file_name, label)
+            if tf_example:
+                writer.write(tf_example.SerializeToString())
+            else:
+                print("{} has mismatched shape".format(file_name))
 
 
-def _bytes_feature(value):
+def _bytes_feature(values):
   """Returns a bytes_list from a string / byte."""
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=values))
 
-def _float_feature(value):
+def _float_feature(values):
   """Returns a float_list from a float / double."""
-  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+  return tf.train.Feature(float_list=tf.train.FloatList(value=values))
 
-def _int64_feature(value):
+def _int64_feature(values):
   """Returns an int64_list from a bool / enum / int / uint."""
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=values))
 
 
 
